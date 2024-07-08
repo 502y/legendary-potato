3b465144ff3cc32f1166d09e57448553a0e69a7a
502y <53784463+502y@users.noreply.github.com>
2024-07-08 14:43:48 +0800

    feat(database): implement singleton pattern and add utility methods
    - Introduce SingletonDecorator to ensure Database class instantiation is singleton.- Add various database operation methods such as query, execute, insert_threat, etc.
    - Update dataSources.xml with new UUID and adjusted JDBC URL reflecting the
      file path change in the database_util.py module.

---
+++ utils/database/database_util.py
@@ -0,0 +1,80 @@
+import os
+import sqlite3
+
+
+# file path is "utils/database/database_util.py"
+class SingletonDecorator:
+    def __init__(self, klass):
+        self.klass = klass
+        self.instance = None
+
+    def __call__(self, *args, **kwds):
+        if self.instance is None:
+            self.instance = self.klass(*args, **kwds)
+        return self.instance
+
+
+@SingletonDecorator
+class Database:
+    def __init__(self):
+        # 获取当前文件的目录
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(current_dir, "code_analysis.db")
+
+        self.conn = sqlite3.connect(db_path, check_same_thread=False)
+
+    def query(self, sql, params=None):
+        cursor = self.conn.cursor()
+        if params:
+            cursor.execute(sql, params)
+        else:
+            cursor.execute(sql)
+        self.conn.commit()
+        return cursor.fetchall()
+
+    def execute(self, sql, params=None):
+        cursor = self.conn.cursor()
+        if params:
+            cursor.execute(sql, params)
+        else:
+            cursor.execute(sql)
+        self.conn.commit()
+        return cursor.fetchall()
+
+    def get_all_threat(self):
+        return self.query("SELECT * FROM Threat")
+
+    def query_by_threat(self, threat: str):
+        return self.query("SELECT * FROM Threat WHERE threat=?", (threat.lower()))
+
+    def insert_threat(self, threat: str, description: str, level: int):
+        self.execute("INSERT INTO Threat (threat,level, description) VALUES (?, ?,?)",
+                     (threat.lower(), level, description))
+
+    def delete_threat_by_name(self, threat: str):
+        return self.execute("DELETE FROM Threat WHERE threat=?", (threat,))
+
+    def update_threat(self, threat: str, description: str, level: int):
+        # 初始化一个空列表来存储SET子句
+        set_clauses = []
+        # 初始化一个空列表来存储参数值
+        params = []
+
+        if description is not None:
+            set_clauses.append("description = ?")
+            params.append(description)
+        if level is not None:
+            set_clauses.append("level = ?")
+            params.append(level)
+
+        if not set_clauses:
+            return
+
+        # 构建完整的UPDATE语句
+        update_query = f"UPDATE Threat SET {', '.join(set_clauses)} WHERE threat = ?"
+
+        params.append(threat.lower())
+        self.execute(update_query, params)
+
+    def close(self):
+        self.conn.close()
