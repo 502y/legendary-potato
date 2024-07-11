3e6e251c45d025a4ff3364efcd8be1c28ea2270b
aqrita <128269049+aqrita@users.noreply.github.com>
2024-07-11 11:49:45 +0800

    Create AstTreeJson.py

---
+++ funcTrace/AstTreeJson.py
@@ -0,0 +1,54 @@
+import json
+from clang.cindex import Index, Config
+
+class AST_Tree_json:
+    def __init__(self, absolute_path, args = None):
+        self.absolute_path = absolute_path
+        self.clang_path = r'..\build\LLVM\bin\libclang.dll'
+        Config.set_library_file(self.clang_path)
+
+        # 设置命令行参数以忽略标准头文件
+        if args is None:
+            args = ['-nostdinc']
+        elif not any(arg.startswith('-nostdinc') for arg in args):
+            args.append('-nostdinc')
+
+        self.AST_Root = Index.create().parse(absolute_path, args).cursor
+
+    def serialize_node(self, cursor):
+        node_dict = {
+            "kind": str(cursor.kind),
+            "location": [cursor.extent.start.line, cursor.extent.start.column,
+                         cursor.extent.end.line, cursor.extent.end.column],
+            "children": []
+        }
+        if cursor.spelling:
+            # print('kind: ', cursor.kind)
+            node_dict["spelling"] = cursor.spelling
+            # print('keywords: ', cursor.spelling)
+            # print('location: ', cursor.extent.start.line, cursor.extent.start.column,
+            #       cursor.extent.end.line, cursor.extent.end.column)
+        for child in cursor.get_children():
+            child_dict = self.serialize_node(child)
+            node_dict["children"].append(child_dict)
+        # (start_line, start_column, end_line, end_column) (起始行, 起始列， 结束行，结束列)
+        return node_dict
+
+    def start(self):
+        string_res = self.serialize_node(self.AST_Root)
+        serialized_json = json.dumps(string_res, indent=4, ensure_ascii=False)
+        return serialized_json
+        # local_time = time.localtime()
+        # date_time = time.strftime("%Y_%m_%d_%H_%M_%S", local_time)
+        # with open('./res_{}.json'.format(date_time), 'w', encoding='utf-8') as file:
+        #     file.write(serialized_json)
+        #     file.close()
+        # 虽然但是它能识别[]{};+-=，不能获取它们的标识符....而且获取不到值....
+        # print(serialized_json)
+
+
+if __name__ == '__main__':
+    path = r'C:/Users/86177/Desktop/test/test.c'
+    ast_obj = AST_Tree_json(path)
+    json_ = ast_obj.start()
+    # print('a')
