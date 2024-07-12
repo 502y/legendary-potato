2264e2fa9d1bccc1d86de5771901d0b1bdc28c6a
502y <53784463+502y@users.noreply.github.com>
2024-07-12 17:13:12 +0800

    refactor(funcTrace): implement singleton pattern and improve AST parsing
    - Add singleton pattern to AST_Tree_json class for global instance management.
    - Update AST tree serialization to include file name and improve child node handling.
    - Integrate LLVMGeneratedModel for AST representation and add utility functions for model manipulation.
    - Refactor main_view_controller to use the updated AST parsing functionality.
    - Fix potential issues with C source file analysis and main function detection.

--- client/controller/main_view_controller.py
+++ client/controller/main_view_controller.py
@@ -1,14 +1,18 @@
 import html
+import json
 import os
 import re
 
 from PyQt5.QtCore import QModelIndex
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QFileDialog, QMainWindow, QMessageBox
+from ordered_set import OrderedSet
 
 from client.controller.database_view_controller import DatabaseEditor
+from client.model.LLVMGeneratedModel import LLVMGeneratedModel, cursor_kind_dict, cursor_kind_ignore_set
 from client.view.custom_treeview import CustomFileSystemModel
 from client.view.main_view import MainWindowView
+from funcTrace.AstTreeJson import AST_Tree_json
 from utils.cmd_utils import compile_and_run
 from utils.file_utils import extract_custom_headers_and_sources
 from utils.str_utils import is_empty_or_whitespace, search_in_str
@@ -18,6 +22,7 @@ class MainWindowViewController(QMainWindow, MainWindowView):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.entry_point = ""
+        self.ast_instance: LLVMGeneratedModel
 
         self.setup_ui(self)
 
@@ -30,6 +35,9 @@ class MainWindowViewController(QMainWindow, MainWindowView):
         self.treeView.doubleClicked.connect(self.get_selected_file_from_tree)
         self.search_button.clicked.connect(self.do_search)
 
+        self.var_set = OrderedSet()
+        self.info_text = ""
+
     def open_File(self):
 
         dialog = QFileDialog()
@@ -38,10 +46,20 @@ class MainWindowViewController(QMainWindow, MainWindowView):
 
         if dialog.exec_():
             selected_file = dialog.selectedFiles()[0]
+            # 判断是否包含C入口
+            with open(selected_file, "r") as file:
+                content = file.read()
+
+                pattern = r"\b(int|void)\b\s+main\s*\(\s*(?:int\s+argc,\s*(?:char\s*\*\s*){1,2}argv\[\])?\s*\)\s*\{.*\}"
+                if len(re.findall(pattern, content)):
+                    QMessageBox.information(self, "警告", "请选择包含main函数的C源文件", QMessageBox.Yes)
+                    return
+
             if selected_file:
                 self.entry_point = selected_file
                 self.statusbar.showMessage(f"项目入口：{selected_file}")
 
+                # 包含头文件、源文件分析
                 custom_headers, custom_sources = extract_custom_headers_and_sources(selected_file)
                 paths = [os.path.abspath(header) for header in custom_headers] + [os.path.abspath(source) for source in
                                                                                   custom_sources]
@@ -59,9 +77,10 @@ class MainWindowViewController(QMainWindow, MainWindowView):
                 self.treeView.setRootIndex(index)
                 self.treeView.expandAll()
 
+                self.show_source_code(selected_file)
 
             else:
-                self.statusbar.showMessage("选择的文件夹无效")
+                self.statusbar.showMessage("选择的文件无效")
                 return
 
     def get_selected_file_from_tree(self, index: QModelIndex):
@@ -69,13 +88,28 @@ class MainWindowViewController(QMainWindow, MainWindowView):
             item = self.model.filePath(index)
             if os.path.isdir(item) or item.endswith(".pyc"):
                 return
-            if not item.endswith(".c") and not item.endswith(".h"):
-                QMessageBox.information(self, "警告", "您选择的不是C源文件或C头文件，程序可能无法正常工作",
+            if not item.endswith(".c"):
+                QMessageBox.information(self, "警告", "您选择的不是C源文件，程序可能无法正常工作",
                                         QMessageBox.Yes)
-            with open(item, "r") as file_:
-                content = file_.read()
-                self.sourceBroswer.setText(
-                    f'<pre><code style="white-space: pre-wrap; font-family: inherit;">{html.escape(content)}</code></pre>')
+            self.show_source_code(item)
+
+    def show_source_code(self, path):
+        with open(path, "r") as file_:
+            content = file_.read()
+            self.sourceBroswer.setText(
+                f'<pre><code style="white-space: pre-wrap; font-family: inherit;">{html.escape(content)}</code></pre>')
+        self.var_set = OrderedSet()
+        var_text = ""
+        try:
+            self.generate_ast(path)
+            self.analyze_ast(self.ast_instance, path)
+
+        except Exception as e:
+            print(e)
+        finally:
+            for item in self.var_set:
+                var_text = var_text + item
+            self.varBrowser.setText(var_text)
 
     def do_search(self):
         content = self.search_line_edit.text()
@@ -131,6 +165,50 @@ class MainWindowViewController(QMainWindow, MainWindowView):
             except Exception as e:
                 self.showError(e)
                 return
+
+    def generate_ast(self, path):
+        ast_obj = AST_Tree_json(path)
+        json_ = ast_obj.start()
+        data = json.loads(json_)
+        try:
+            self.ast_instance = LLVMGeneratedModel.from_dict(data)
+        except Exception as e:
+            print(e)
+
+    def analyze_ast(self, ast_ins, file):
+        while True:
+            has_child = False
+            if ast_ins.file == file and ast_ins.spelling != file:
+                if ast_ins.kind in cursor_kind_dict:
+                    if ast_ins and ast_ins.spelling is not None and ast_ins.kind is not None and ast_ins.file is not None and ast_ins.location is not None:
+                        # if ast_ins.kind is CursorKind.VAR_DECL:
+                        #    self.var_set.add(f"变量声明：{ast_ins.spelling} line {ast_ins.location[0]}<br>")
+                        # elif ast_ins.kind is CursorKind.TYPE_REF:
+                        #     self.var_set.add(f"变量类型：{ast_ins.spelling}<br><br>")
+                        # elif ast_ins.kind is CursorKind.FUNCTION_DECL:
+                        #     self.var_set.add(f"函数声明：{ast_ins.spelling}() line {ast_ins.location[0]}<br><br>")
+                        # elif ast_ins.kind is CursorKind.PARM_DECL:
+                        #     self.var_set.add(f"参数声明：{ast_ins.spelling}() line {ast_ins.location[0]}<br><br>")
+                        # elif ast_ins.kind is CursorKind.MEMBER_REF:
+                        #     self.var_set.add(f"成员声明：{ast_ins.spelling} line {ast_ins.location[0]}<br><br>")
+                        # elif ast_ins.kind is CursorKind.CALL_EXPR:
+                        #     self.var_set.add(f"函数调用：{ast_ins.spelling} line {ast_ins.location[0]}<br><br>")
+                        if ast_ins.kind in cursor_kind_ignore_set:
+                            pass
+                        else:
+                            self.var_set.add(
+                                f"{cursor_kind_dict.get(ast_ins.kind)}：{ast_ins.spelling} line {ast_ins.location[0]}<br><br>")
+                        print(
+                            f"{cursor_kind_dict.get(ast_ins.kind)}：{ast_ins.spelling} line {ast_ins.location[0]}<br><br>")
+
+            if len(ast_ins.children) != 0:
+                has_child = True
+            if not has_child:
+                break
+            else:
+                for child in ast_ins.children:
+                    self.analyze_ast(child, file)
+                break
 
 
 def handle_non_match(string: str) -> str:
--- funcTrace/AstTreeJson.py
+++ funcTrace/AstTreeJson.py
@@ -1,25 +1,39 @@
 import json
+
 from clang.cindex import Index, Config
 
+from client.model.LLVMGeneratedModel import LLVMGeneratedModel
+
+
 class AST_Tree_json:
-    def __init__(self, absolute_path, args = None):
-        self.absolute_path = absolute_path
-        self.clang_path = r'..\build\LLVM\bin\libclang.dll'
-        Config.set_library_file(self.clang_path)
+    _instance = None  # 用于存储单例的私有类变量
 
-        # 设置命令行参数以忽略标准头文件
-        if args is None:
-            args = ['-nostdinc']
-        elif not any(arg.startswith('-nostdinc') for arg in args):
-            args.append('-nostdinc')
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
-        self.AST_Root = Index.create().parse(absolute_path, args).cursor
+    def __init__(self, absolute_path, args=None):
+        if not hasattr(self, '_initialized'):  # 防止重复初始化
+            self._initialized = True
+            self.absolute_path = absolute_path
+            self.clang_path = r'E:\LLVM\bin\libclang.dll'
+            Config.set_library_file(self.clang_path)
+
+            # 设置命令行参数以忽略标准头文件
+            if args is None:
+                args = ['-nostdinc']
+            elif not any(arg.startswith('-nostdinc') for arg in args):
+                args.append('-nostdinc')
+
+            self.AST_Root = Index.create().parse(absolute_path, args).cursor
 
     def serialize_node(self, cursor):
         node_dict = {
             "kind": str(cursor.kind),
             "location": [cursor.extent.start.line, cursor.extent.start.column,
                          cursor.extent.end.line, cursor.extent.end.column],
+            "file": cursor.extent.start.file.name if cursor.extent.start.file else '',
             "children": []
         }
         if cursor.spelling:
@@ -46,9 +60,16 @@ class AST_Tree_json:
         # 虽然但是它能识别[]{};+-=，不能获取它们的标识符....而且获取不到值....
         # print(serialized_json)
 
+    def get_AST_Root(self):
+        return self.AST_Root
+
 
 if __name__ == '__main__':
-    path = r'C:/Users/86177/Desktop/test/test.c'
+    path = r"C:\Users\13238\Downloads\Compressed\CJAG-master\CJAG-master\cjag.c"
     ast_obj = AST_Tree_json(path)
     json_ = ast_obj.start()
-    # print('a')
+    data = json.loads(json_)
+    instance = LLVMGeneratedModel.from_dict(data)
+    with open("report.json", 'w') as f:
+        f.write(json_)
+    # print(json_)
